b1e024a7f60f6bdac83f0bb8fb01076c066e7869
namkcits <132549602+namkcits@users.noreply.github.com>
2023-08-14 14:43:01 -0700

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,73 @@
+import secrets
+import json
+from qiskit import QuantumCircuit, execute, Aer
+from datetime import datetime  # Don't forget to import datetime
+
+NUM_QUBITS = 10  # Number of qubits for quantum communication
+IN_EDGE_NUM_QUBITS = 1  # Number of qubits in the in-edge circuit (modify as needed)
+used_names = set()  # To keep track of used owner names
+
+class QCChain:
+    def __init__(self):
+        self.qubit_id_counter = -1
+        self.user_qubits = {}
+
+    def mine_qubit(self, qubit_state, wallet_address):
+        qubit_id = self.qubit_id_counter + 1
+        circuit = QuantumCircuit(1)
+        circuit.h(0)
+        circuit.measure_all()
+
+        backend = Aer.get_backend('qasm_simulator')
+        job = execute(circuit, backend, shots=1)
+        result = job.result()
+        counts = result.get_counts(circuit)
+        qubit_state = list(counts.keys())[0]
+
+        if wallet_address not in self.user_qubits:
+            self.user_qubits[wallet_address] = []
+
+        mined_qubit = {
+            'qubit_id': qubit_id,
+            'qubit_state': qubit_state,
+            'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        }
+        self.user_qubits[wallet_address].append(mined_qubit)
+
+        self.qubit_id_counter += 1
+
+        return qubit_id, qubit_state
+
+def create_entangled_qubits(num_qubits):
+    circuit = QuantumCircuit(num_qubits, num_qubits)
+    for i in range(1, num_qubits):
+        circuit.cx(0, i)
+    return circuit
+
+# ... (rest of the quantum functions: bb84_protocol, quantum_teleportation)
+
+if __name__ == "__main__":
+    qc_chain = QCChain()
+
+    backend = Aer.get_backend('qasm_simulator')  # You can choose the backend here
+
+    # ... (rest of the initialization steps)
+
+    owner_name = "nam-kcits"  # Replace dynamically generated name with "nam-kcits"
+
+    entangled_circuit = create_entangled_qubits(NUM_QUBITS)
+    in_edge_circuit = QuantumCircuit(IN_EDGE_NUM_QUBITS)
+    qubit_index_to_compose = 0
+
+    combined_circuit = entangled_circuit.compose(in_edge_circuit, qubits=[qubit_index_to_compose], inplace=False)
+
+    # ... (rest of the measurements and quantum operations)
+
+    # Store QCChain data
+    qc_chain_data = {
+        'owner_data': owner_name,
+        'user_qubits': qc_chain.user_qubits
+    }
+
+    with open('qc_chain_results.json', 'w') as file:
+        json.dump(qc_chain_data, file)
